40448e214b79c74f61084bef9c68d6a9f6263b59
afeiship <1290657123@qq.com>
2017-08-04 21:23:27 +0800

    issue: finish hello world

---
+++ 001-imooc-lava-01/imooc/src/com/imooc/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.imooc;
+
+/**
+ * Created by feizheng on 8/4/17.
+ */
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("Hello world!");
+    }
+}
